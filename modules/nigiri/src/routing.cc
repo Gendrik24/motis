c7cc9a4ea103b2787714b23019fb0129cbe1dd0b
gendrik24 <hendriksiemund@gmx.de>
2023-04-07 00:59:06 +0200

    Nigiri Version Update. Added Reconstruction time statistic to raptor implementation.

--- modules/nigiri/src/routing.cc
+++ modules/nigiri/src/routing.cc
@@ -51,6 +51,8 @@ mm::msg_ptr to_routing_response(
       fbb.CreateVector(std::vector<fbs::Offset<StatisticsEntry>>{
           CreateStatisticsEntry(fbb, fbb.CreateString("routing_time_ms"),
                                 stats.n_routing_time_),
+          CreateStatisticsEntry(fbb, fbb.CreateString("reconstruction_time_ms"),
+                                stats.n_reconstruction_time),
           CreateStatisticsEntry(fbb, fbb.CreateString("lower_bounds_time_ms"),
                                 stats.lb_time_),
           CreateStatisticsEntry(fbb, fbb.CreateString("footpaths_visited"),
